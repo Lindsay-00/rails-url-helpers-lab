b4ca2c73602328216db6a83af8133a44bc63466d
Lindsay <lindsay.chen@yale.edu>
2021-07-08 04:32:52 +0800

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,16 @@
 class Student < ActiveRecord::Base
+  # attr_reader :active
+  
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  # def active (status = false)
+  #   @status = status
+  # end
+
+  # def active=(new_status)
+  #   @status = new_status
+  # end
+
 end
